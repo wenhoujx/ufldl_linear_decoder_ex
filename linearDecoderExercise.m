e3393689792a0124bc4a4c4049fbebb669c03919
= <=wenhoujx@gmail.com>
2013-05-30 10:32:49 -0400

    minor change

--- linearDecoderExercise.m
+++ linearDecoderExercise.m
@@ -1,16 +1,8 @@
-%% CS294A/CS294W Linear Decoder Exercise
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear decoder exericse. For this exercise, you will only need to modify
-%  the code in sparseAutoencoderLinearCost.m. You will not need to modify
-%  any code in this file.
-
 %%======================================================================
 %% STEP 0: Initialization
 %  Here we initialize some parameters used for the exercise.
+tic; 
+
 
 imageChannels = 3;     % number of channels (rgb, so 3)
 
@@ -27,54 +19,6 @@ beta = 5;              % weight of sparsity penalty term
 
 epsilon = 0.1;	       % epsilon for ZCA whitening
 
-%%======================================================================
-%% STEP 1: Create and modify sparseAutoencoderLinearCost.m to use a linear decoder,
-%          and check gradients
-%  You should copy sparseAutoencoderCost.m from your earlier exercise 
-%  and rename it to sparseAutoencoderLinearCost.m. 
-%  Then you need to rename the function from sparseAutoencoderCost to
-%  sparseAutoencoderLinearCost, and modify it so that the sparse autoencoder
-%  uses a linear decoder instead. Once that is done, you should check 
-% your gradients to verify that they are correct.
-
-% NOTE: Modify sparseAutoencoderCost first!
-
-% To speed up gradient checking, we will use a reduced network and some
-% dummy patches
-
-debugHiddenSize = 5;
-debugvisibleSize = 8;
-patches = rand([8 10]);
-theta = initializeParameters(debugHiddenSize, debugvisibleSize); 
-
-[cost, grad] = sparseAutoencoderLinearCost(theta, debugvisibleSize, debugHiddenSize, ...
-                                           lambda, sparsityParam, beta, ...
-                                           patches);
-
-% Check gradients
-numGrad = computeNumericalGradient( @(x) sparseAutoencoderLinearCost(x, debugvisibleSize, debugHiddenSize, ...
-                                                  lambda, sparsityParam, beta, ...
-                                                  patches), theta);
-
-% Use this to visually compare the gradients side by side
-disp([numGrad grad]); 
-
-
-diff = norm(numGrad-grad)/norm(numGrad+grad);
-% Should be small. In our implementation, these values are usually less than 1e-9.
-disp(diff); 
-
-assert(diff < 1e-9, 'Difference too large. Check your gradient computation again');
-
-% NOTE: Once your gradients check out, you should run step 0 again to
-%       reinitialize the parameters
-%}
-
-%%======================================================================
-%% STEP 2: Learn features on small patches
-%  In this step, you will use your sparse autoencoder (which now uses a 
-%  linear decoder) to learn features on small patches sampled from related
-%  images.
 
 %% STEP 2a: Load patches
 %  In this step, we load 100k patches sampled from the STL10 dataset and
@@ -82,7 +26,6 @@ assert(diff < 1e-9, 'Difference too large. Check your gradient computation again
 
 load data/stlSampledPatches.mat
 
-% displayColorNetwork(patches(:, 1:100));
 
 %% STEP 2b: Apply preprocessing
 %  In this sub-step, we preprocess the sampled patches, in particular, 
@@ -104,7 +47,6 @@ sigma = patches * patches' / numPatches;
 ZCAWhite = u * diag(1 ./ sqrt(diag(s) + epsilon)) * u';
 patches = ZCAWhite * patches;
 
-displayColorNetwork(patches(:, 1:100));
 
 %% STEP 2c: Learn features
 %  You will now use your sparse autoencoder (with linear decoder) to learn
@@ -120,20 +62,15 @@ options.Method = 'lbfgs';
 options.maxIter = 400;
 options.display = 'on';
 
-% % % % [optTheta, cost] = minFunc( @(p) sparseAutoencoderLinearCost(p, ...
-% % % %                                    visibleSize, hiddenSize, ...
-% % % %                                    lambda, sparsityParam, ...
-% % % %                                    beta, patches), ...
-% % % %                               theta, options);
-% % % % 
-% % % % % Save the learned features and the preprocessing matrices for use in 
-% % % % % the later exercise on convolution and pooling
-% % % % fprintf('Saving learned features and preprocessing matrices...\n');                          
-% % % % save('STL10Features.mat', 'optTheta', 'ZCAWhite', 'meanPatch');
-% % % % fprintf('Saved\n');
+[optTheta, cost] = minFunc( @(p) sparseAutoencoderLinearCost(p, ...
+                                   visibleSize, hiddenSize, ...
+                                   lambda, sparsityParam, ...
+                                   beta, patches), ...
+                              theta, options);
 
-%% STEP 2d: Visualize learned features
-
-W = reshape(optTheta(1:visibleSize * hiddenSize), hiddenSize, visibleSize);
-b = optTheta(2*hiddenSize*visibleSize+1:2*hiddenSize*visibleSize+hiddenSize);
-displayColorNetwork( (W*ZCAWhite)');
+% Save the learned features and the preprocessing matrices for use in 
+% the later exercise on convolution and pooling
+fprintf('Saving learned features and preprocessing matrices...\n');                          
+save('STL10Features.mat', 'optTheta', 'ZCAWhite', 'meanPatch');
+fprintf('Saved\n');
+toc 
